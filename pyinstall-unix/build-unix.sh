be66b35bb190e9aebd209729bd4d62a73c952bd6
Quentin Soubeyran <45202794+QuentinSoubeyran@users.noreply.github.com>
2020-10-10 18:40:29 +0200

    added version to builds

--- pyinstall-unix/build-unix.sh
+++ pyinstall-unix/build-unix.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+HERE=$(pwd)
+cd ..
+VERSION=$(python3 -c "from src import version; print(version.__version__)")
+cd "$HERE"
 rm -rf build/
 rm -rf dist/
-pyinstaller --clean --name "saje" --onefile ../main.py
+pyinstaller --clean --name "saje-linux-$VERSION" --onefile ../main.py
--- pyinstall-win/build-win.ps1
+++ pyinstall-win/build-win.ps1
@@ -1,3 +1,7 @@
+$HERE=Get-Location | select -ExpandProperty Path
 rm -rf build/
 rm -rf dist/
-pyinstaller --clean --name "saje" --onefile ../main.py
+cd ..
+$VERSION=python3 -c "from src import version; print(version.__version__)"
+cd "$HERE"
+pyinstaller --clean --name "saje-$VERSION" --onefile ../main.py
