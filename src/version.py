8b6c9a6ba11198ab79b42170ffbe81f519baed85
Quentin Soubeyran <45202794+QuentinSoubeyran@users.noreply.github.com>
2020-10-14 22:46:02 +0200

    increased version number

--- src/version.py
+++ src/version.py
@@ -7,6 +7,6 @@ Version file for the SAJE project
 __author__ = "Quentin Soubeyran"
 __copyright__ = "Copyright 2020, SAJE project"
 __license__ = "MIT"
-__version__ = "0.1.3"
+__version__ = "0.1.5"
 __maintainer__ = "Quentin Soubeyran"
 __status__ = "alpha"
